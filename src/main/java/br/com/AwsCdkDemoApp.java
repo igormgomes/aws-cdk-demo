17f5528387d8d74d8757874d24b716d964967e4c
igormgomes <igormgomes94@gmail.com>
2022-02-12 18:18:42 -0300

    first commit

---
+++ src/main/java/br/com/AwsCdkDemoApp.java
@@ -0,0 +1,11 @@
+package br.com;
+
+import software.amazon.awscdk.App;
+
+public class AwsCdkDemoApp {
+
+    public static void main(final String[] args) {
+        App app = new App();
+        app.synth();
+    }
+}
